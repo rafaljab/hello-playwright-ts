6c576d4abb2d5f90413d779fa2d1984890d80a54
Rafal Jablonski <jablonski.rafal@gmail.com>
2023-03-09 19:00:59 +0100

    test: add POM and tests for login page

---
+++ pom/login.ts
@@ -0,0 +1,28 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class LoginPage {
+  url = 'https://rafaljab.github.io/gui-automation-playground/login';
+  readonly page: Page;
+  readonly emailField: Locator;
+  readonly passField: Locator;
+  readonly loginBtn: Locator;
+  readonly alertNotification: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.emailField = page.locator('#email');
+    this.passField = page.locator('#password');
+    this.loginBtn = page.locator('button[type="submit"]');
+    this.alertNotification = page.locator('div[role="alert"]');
+  }
+
+  async navigate() {
+    await this.page.goto(this.url);
+  }
+
+  async login(email: string, password: string) {
+    await this.emailField.fill(email);
+    await this.passField.fill(password);
+    await this.loginBtn.click();
+  }
+}
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,47 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pom/login';
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://rafaljab.github.io/gui-automation-playground');
+test('login with correct data', async ({ page }) => {
+  // Given
+  const email = 'admin@example.com';
+  const password = 'admin123';
+  const loginPage = new LoginPage(page);
 
-  await page.locator('#email').fill('admin@example.com')
-  await page.locator('#password').fill('admin123')
-  await page.locator('button[type="submit"]').click()
+  // When
+  await loginPage.navigate();
+  await loginPage.login(email, password);
 
+  // Then
   await expect(page).toHaveURL('https://rafaljab.github.io/gui-automation-playground');
-  await expect(page.locator('div[role="alert"]')).toBeHidden();
+  await expect(loginPage.alertNotification).toBeHidden();
+});
+
+test('login with incorrect email', async ({ page }) => {
+  // Given
+  const email = 'test@example.com';
+  const password = 'admin123';
+  const loginPage = new LoginPage(page);
+
+  // When
+  await loginPage.navigate();
+  await loginPage.login(email, password);
+
+  // Then
+  await expect(page).toHaveURL(loginPage.url);
+  await expect(loginPage.alertNotification).toBeVisible();
+});
+
+test('login with incorrect password', async ({ page }) => {
+  // Given
+  const email = 'admin@example.com';
+  const password = 'test';
+  const loginPage = new LoginPage(page);
+
+  // When
+  await loginPage.navigate();
+  await loginPage.login(email, password);
+
+  // Then
+  await expect(page).toHaveURL(loginPage.url);
+  await expect(loginPage.alertNotification).toBeVisible();
 });
