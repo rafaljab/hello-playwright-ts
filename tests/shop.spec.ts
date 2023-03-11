e4cff725570b6ece71f2708359df20b944dfda5b
Rafal Jablonski <jablonski.rafal@gmail.com>
2023-03-11 17:35:54 +0100

    test: add more tests for shop feature

--- tests/shop.spec.ts
+++ tests/shop.spec.ts
@@ -1,5 +1,121 @@
 import { test, expect } from '../base';
 
+test('open empty cart', async ({ shopPageAuthenticated }) => {
+  // Given
+  const shopPage = shopPageAuthenticated;
+
+  await expect(shopPage.emptyCartText).not.toBeVisible();
+
+  // When
+  await shopPage.viewCart();
+
+  // Then
+  await expect(shopPage.emptyCartText).toBeVisible();
+  await expect(shopPage.placeOrderBtn).toBeDisabled();
+});
+
+const orders = [
+  {
+    productName: 'iPhone 9',
+    totalItems: 1,
+    totalPrice: '$549.00'
+  },
+  {
+    productName: 'iPhone X',
+    totalItems: 1,
+    totalPrice: '$899.00'
+  },
+  {
+    productName: 'Samsung Universe 9',
+    totalItems: 1,
+    totalPrice: '$1,249.00'
+  }
+];
+for (const order of orders) {
+  test(`add product to cart - ${order.productName}`, async ({ shopPageAuthenticated }) => {
+    // Given
+    const shopPage = shopPageAuthenticated;
+
+    await expect(shopPage.productCardBtn(order.productName)).toHaveAttribute('title', 'Add Product');
+    await expect(shopPage.shopHeaderTotalItems).toHaveText('Total Items: 0');
+    await expect(shopPage.shopHeaderTotalPrice).toHaveText('Total Price: $0.00');
+
+    // When
+    await shopPage.addProductToCart(order.productName);
+
+    // Then
+    await expect(shopPage.productCardBtn(order.productName)).toHaveAttribute('title', 'Product In Cart');
+    await expect(shopPage.shopHeaderTotalItems).toHaveText(`Total Items: ${order.totalItems}`);
+    await expect(shopPage.shopHeaderTotalPrice).toHaveText(`Total Price: ${order.totalPrice}`);
+  });
+}
+
+test('add multiple products to cart', async ({ shopPageAuthenticated }) => {
+  // Given
+  const shopPage = shopPageAuthenticated;
+
+  await expect(shopPage.shopHeaderTotalItems).toHaveText('Total Items: 0');
+  await expect(shopPage.shopHeaderTotalPrice).toHaveText('Total Price: $0.00');
+
+  // When
+  await shopPage.addProductToCart('iPhone 9');
+  await shopPage.addProductToCart('iPhone X');
+  await shopPage.addProductToCart('Samsung Universe 9');
+  await shopPage.addProductToCart('iPhone 9');
+
+  // Then
+  await expect(shopPage.shopHeaderTotalItems).toHaveText('Total Items: 4');
+  await expect(shopPage.shopHeaderTotalPrice).toHaveText('Total Price: $3,246.00');
+});
+
+test('change number of product items in cart', async ({ shopPageAuthenticated }) => {
+  // Given
+  const shopPage = shopPageAuthenticated;
+
+  const productName = 'iPhone 9';
+
+  await shopPage.addProductToCart(productName);
+  await shopPage.viewCart();
+
+  await expect(shopPage.shopHeaderTotalItems).toHaveText('Total Items: 1');
+  await expect(shopPage.shopHeaderTotalPrice).toHaveText('Total Price: $549.00');
+  await expect(shopPage.productCartItemBadge(productName)).toHaveText('1');
+  await expect(shopPage.productCartItemQuantityDropdown(productName)).toHaveText('1');
+  await expect(shopPage.productCartItemSubtotalPrice(productName)).toHaveText('Subtotal Price: $549.00');
+
+  // When
+  await shopPage.changeQuantityOfProduct(productName, 10);
+
+  // Then
+  await expect(shopPage.shopHeaderTotalItems).toHaveText('Total Items: 10');
+  await expect(shopPage.shopHeaderTotalPrice).toHaveText('Total Price: $5,490.00');
+  await expect(shopPage.productCartItemBadge(productName)).toHaveText('10');
+  await expect(shopPage.productCartItemQuantityDropdown(productName)).toHaveText('10');
+  await expect(shopPage.productCartItemSubtotalPrice(productName)).toHaveText('Subtotal Price: $5,490.00');
+});
+
+test('remove product item from cart', async ({ shopPageAuthenticated }) => {
+  // Given
+  const shopPage = shopPageAuthenticated;
+
+  const productName = 'iPhone 9';
+
+  await shopPage.addProductToCart(productName);
+  await shopPage.viewCart();
+
+  await expect(shopPage.productCartItem(productName)).toBeVisible();
+  await expect(shopPage.emptyCartText).not.toBeVisible();
+  await expect(shopPage.placeOrderBtn).toBeEnabled();
+
+  // When
+  await shopPage.removeProductFromCart(productName);
+
+  // Then
+  await expect(shopPage.productCartItem(productName)).not.toBeVisible();
+  await expect(shopPage.emptyCartText).toBeVisible();
+  await expect(shopPage.placeOrderBtn).toBeDisabled();
+});
+
 test('place order', async ({ shopPage }) => {
   // Given
   const productsToOrder = [
