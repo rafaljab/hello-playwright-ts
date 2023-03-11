28a26829741a61fa5332b33248d303b950f37de1
Rafal Jablonski <jablonski.rafal@gmail.com>
2023-03-11 17:35:19 +0100

    feat: add fixtures pageAuthenticated and shopPageAuthenticated

--- base.ts
+++ base.ts
@@ -1,13 +1,16 @@
-import { test as base } from '@playwright/test';
+import { Page, test as base} from '@playwright/test';
 import { LoginPage } from './pom/login';
 import { LeftMenuFragment } from './pom/left-menu';
 import { ShopPage } from './pom/shop';
+import authenticatedStateJson from 'tests/test-data/authenticated-state.json';
 
 type MyFixtures = {
   loginPage: LoginPage;
   login: Promise<void>;
+  pageAuthenticated: Page;
   leftMenuFragment: LeftMenuFragment;
   shopPage: ShopPage;
+  shopPageAuthenticated: ShopPage;
 };
 
 export const test = base.extend<MyFixtures>({
@@ -20,6 +23,12 @@ export const test = base.extend<MyFixtures>({
     await loginPage.login('admin@example.com', 'admin123');
     await use(null);
   },
+  pageAuthenticated: async ({ browser, baseURL }, use) => {
+    const context = await browser.newContext({ storageState: replaceOriginInState(authenticatedStateJson, baseURL) });
+    const page = await context.newPage();
+    await use(page);
+    await context.close();
+  },
   leftMenuFragment: async ({ page, login }, use) => {
     const leftMenuFragment = new LeftMenuFragment(page);
     await use(leftMenuFragment);
@@ -27,7 +36,17 @@ export const test = base.extend<MyFixtures>({
   shopPage: async ({ leftMenuFragment }, use) => {
     const shopPage = await leftMenuFragment.clickShopLink();
     await use(shopPage);
+  },
+  shopPageAuthenticated: async ({ pageAuthenticated }, use) => {
+    const shopPageAuthenticated = new ShopPage(pageAuthenticated);
+    await shopPageAuthenticated.navigate();
+    await use(shopPageAuthenticated);
   }
 });
+
+function replaceOriginInState(data, baseUrl) {
+  data.origins[0].origin = baseUrl
+  return data
+}
 
 export { expect } from '@playwright/test';
