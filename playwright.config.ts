ed5db61071a55daa6cf69d11f2956e161c399148
Rafal Jablonski <jablonski.rafal@gmail.com>
2024-07-29 18:37:39 +0200

    chore: update paths for results and reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,8 +16,13 @@ export const authUserStorageStateFile = STORAGE_STATE_PATH + "authenticated-user
  */
 export default defineConfig({
   testDir: "./src/tests",
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  outputDir: "./results/playwright-results",
+
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
+
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -25,22 +30,30 @@ export default defineConfig({
      */
     timeout: 5000,
   },
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { open: "never" }]],
+  reporter: [["html", { outputFolder: "./reports/html-report", open: "never" }]],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
+
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL,
+
     headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -51,53 +64,14 @@ export default defineConfig({
     },
   },
 
-  /* Configure projects for major browsers */
   projects: [
     { name: "setup", testMatch: /.*\.setup\.ts/ },
 
     {
-      name: "chromium",
+      name: "gui-chromium",
+      testDir: "./src/tests/gui/",
       use: { ...devices["Desktop Chrome"], storageState: authUserStorageStateFile },
       dependencies: ["setup"],
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    //
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { channel: 'chrome' },
-    // },
   ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 });
