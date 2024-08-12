71debe3b1fbbc5b12789635a6f0771aa59c43669
Rafal Jablonski <jablonski.rafal@gmail.com>
2024-08-13 00:08:07 +0200

    refactor: left-menu fragment

--- src/pages/gui/fragments/left-menu.fragment.ts
+++ src/pages/gui/fragments/left-menu.fragment.ts
@@ -1,9 +1,6 @@
 import { type Page, type Locator } from "@playwright/test";
 import { BaseFragment } from "@base/gui/base.fragment";
 import { type BasePage } from "@base/gui/base.page";
-import { ShopPage } from "@pages/gui/shop.page";
-import { TodosPage } from "@pages/gui/todos.page";
-import { HomePage } from "@pages/gui/home.page";
 
 export class LeftMenuFragment extends BaseFragment {
   readonly homeLink: Locator;
@@ -20,16 +17,13 @@ export class LeftMenuFragment extends BaseFragment {
 
   async clickHomeLink() {
     await this.homeLink.click();
-    return new HomePage(this.page);
   }
 
   async clickShopLink() {
     await this.shopLink.click();
-    return new ShopPage(this.page);
   }
 
   async clickTodosLink() {
     await this.todosLink.click();
-    return new TodosPage(this.page);
   }
 }
